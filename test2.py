18119ae34990a471975414a9daa977bd62034209
Jose Gabriel Alves <jose.alves2@gmail.com>
2017-11-12 17:53:52 -0200

    character class, sabin running, working cursor

--- test2.py
+++ test2.py
@@ -1,5 +1,6 @@
 import pygame
 import time
+import spritesheet
 
 pygame.init()
 
@@ -15,20 +16,44 @@ gray = (200, 200, 200)
 gameDisplay = pygame.display.set_mode((display_width,display_height))
 pygame.display.set_caption('test')
 clock = pygame.time.Clock()
+WALKANIMATION = pygame.USEREVENT+1
+pygame.time.set_timer(WALKANIMATION, 100)
 
 bg1 = pygame.image.load('background.png').convert()
 bg2 = pygame.image.load('background.png').convert()
+class charactersprite:
+    def __init__(self, name, filename):
+        self.name = name
+        self.spritelist = []
+        sheet = spritesheet.spritesheet(filename)
+        self.spritelist = sheet.load_strip((0,0,96,96), 13, colorkey=(255, 255, 255))
+        self.walkanim = [2,0,1,0]
+        self.a = 0
+    def walk(self):
+        if self.a == 3:
+            self.a = 0
+        else:
+            self.a += 1
 
+        return self.a
+
+
+
+sabin = charactersprite('sabin', 'sabin_sprite.png')
+
+
+
+sabin_sprites = spritesheet.spritesheet('sabin_sprite.png')
+sabin_idle = sabin_sprites.image_at((0,0,96, 96))
 
 battleMenu = ['Attack','Defend','Magic','Item']
+
 cursor = pygame.image.load('Cursor.png')
 
 
 
 
-
 class menu:
-
     def __init__(self, options, z, w):
         self.eixo_x = z
         self.eixo_y = w
@@ -71,20 +96,6 @@ def game_loop():
     x = 0
 
     while not gameExit:
-
-        for event in pygame.event.get():
-
-            if event.type == pygame.QUIT:
-
-                pygame.quit()
-                quit()
-            if event.type == pygame.KEYUP:
-                if event.key == pygame.K_DOWN and battmenu.pointer < len(battmenu.optCoordinates)-1:
-                    battmenu.pointer += 1
-
-                if event.key == pygame.K_UP and battmenu.pointer > 0:
-                    battmenu.pointer -= 1
-
         gameDisplay.blit(bg1, (x, 0))
         gameDisplay.blit(bg2, (x-display_width, 0))
 
@@ -93,12 +104,27 @@ def game_loop():
         pygame.draw.line(gameDisplay, gray, (250,400),(250,600), 9)
         battmenu.createmenu()
         battmenu.menucursor()
-
+        gameDisplay.blit(sabin.spritelist[sabin.walkanim[sabin.a]], (300, 230))
 
         pygame.display.update()
         x+=10
         if x >= display_width:
             x = 0
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+
+                pygame.quit()
+                quit()
+            if event.type == WALKANIMATION:
+                sabin.walk()
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_DOWN and battmenu.pointer < len(battmenu.optCoordinates)-1:
+                    battmenu.pointer += 1
+
+                if event.key == pygame.K_UP and battmenu.pointer > 0:
+                    battmenu.pointer -= 1
+
 
         clock.tick(60)
 
